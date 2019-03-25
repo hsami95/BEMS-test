0c44b60a8eb692f95d8a0b42595d8ec5ad3e2be4
Nauman Sharif <nauman.pucit@gmail.com>
2019-03-25 20:44:09 +0500

    master

--- BEMS/settings.py
+++ BEMS/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BEMShop',
+    'BEMShop.apps.BemshopConfig',
 #    'django.contrib.sites',
 ]
 
